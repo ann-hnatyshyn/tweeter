50fcae0606ed4c54ee71de20607da1f73c960d3d
Ann <a.m.hnatyshyn@gmail.com>
2024-06-16 15:04:50 -0300

    add tweet box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,32 @@
+import $ from 'jquery';
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
+
 $(document).ready(function() {
-  console.log("composer-char-counter.js is loaded");
+  console.log('Document is ready');
 
-  $('.textarea').on('input', function() {
-    const maxLength = 140;
-    const currentLength = $(this).val().length;
-    const remainingLength = maxLength - currentLength;
-
-    const counter = $(this).closest('.new-tweet').find('.counter');
-    counter.text(remainingLength);
-
-    if (remainingLength < 0) {
-      counter.addClass('over-limit');
-    } else {
-      counter.removeClass('over-limit');
-    }
-  });
+  $("#btn").on('click', function() {
+  console.log(this); //The this keyword is a reference to the button
 });
+
+$("#btn").on('click', () => {
+  console.log(this); //The this keyword here refers to something else!
+});
+});
+
+
+// $('.textarea').on('input', function() {
+//   const maxLength = 140;
+//   const currentLength = $(this).val().length;
+//   const remainingLength = maxLength - currentLength;
+
+//   const counter = $(this).closest('.new-tweet').find('.counter');
+//   counter.text(remainingLength);
+
+//   if (remainingLength < 0) {
+//     counter.addClass('over-limit');
+//   } else {
+//     counter.removeClass('over-limit');
+//   }
+// });
