6468a0eac4ed07946522399a1ef9b88a13d03bb9
Ann <a.m.hnatyshyn@gmail.com>
2024-06-17 19:46:45 -0300

    AJAX to fetch GET

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 import $ from 'jquery';
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 
 const renderTweets = function(tweets) {
@@ -65,4 +40,58 @@ const createTweetElement = function(tweet) {
 
   return $tweet;
 };
+
 renderTweets(data);
+
+$(document).$(function() { {
+
+  const $form = $("#new-tweet-form");
+
+  $form.on("submit", function(event) {
+
+    event.preventDefault();
+    
+    const serializedData = $(this).serialize();
+
+    $.ajax({
+      type: "POST",
+      url: "/tweets",
+      data: serializedData,
+      success: function(response) {
+        console.log("Tweet successfully submitted:", response);
+     
+        $("#tweets-container").prepend(createTweetElement(response));
+      },
+      error: function(error) {
+       
+        console.error("Error submitting tweet:", error);
+      }
+    });
+  });
+});
+
+const loadTweets = function() {
+
+};
+
+function loadTweets() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:8080/tweets",
+    success: function(tweets) {
+      console.log("Tweets fetched successfully:", tweets);
+      // Clear the tweets container
+      $("#tweets").empty();
+
+      for (const tweet of tweets) {
+        const $tweetElement = createTweetElement(tweet);
+        $("#tweets").append($tweetElement);
+      }
+    },
+    error: function(error) {
+      console.error("Error fetching tweets:", error);
+    }
+  });
+}
+
+loadTweets();
